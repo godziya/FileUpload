07a332117942c0b433769721a14497990e7b4bb8
Anonymo <Anonymo@10.84.240.160>
2015-05-06 10:42:20 +0200

    Primer commit

---
+++ src/java/daw/DemoBean.java
@@ -0,0 +1,101 @@
+package daw;
+
+
+import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import javax.servlet.http.Part;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author ramki
+ */
+@Named
+@ViewScoped
+public class DemoBean implements Serializable  {
+
+    private Part file1;
+    private Part file2;
+    private File file;
+    private String foto;
+    private Part uploadedFile;
+
+    public Part getFile1() {
+        return file1;
+    }
+
+    public void setFile1(Part file1) {
+        this.file1 = file1;
+    }
+
+    public Part getFile2() {
+        return file2;
+    }
+
+    public void setFile2(Part file2) {
+        this.file2 = file2;
+    }
+
+    public String upload() throws IOException {
+        //Tomcat
+        // file1.write("C:\\files\\"+getFilename(file1));
+        final FacesContext facesContext=FacesContext.getCurrentInstance();
+        final ExternalContext externalContext=facesContext.getExternalContext();
+        InputStream inputStream = file1.getInputStream();
+        FileOutputStream outputStream = new FileOutputStream(externalContext.getRealPath("/")+"/resources/images/" + getFilename(file1));
+        byte[] buffer = new byte[4096];
+        int bytesRead = 0;
+        while (true) {
+            bytesRead = inputStream.read(buffer);
+            if (bytesRead > 0) {
+                outputStream.write(buffer, 0, bytesRead);
+            } else {
+                break;
+            }
+        }
+       outputStream.close();
+       inputStream.close();
+           setFoto(getFilename(file1));
+        return "";
+    }
+
+    private static String getFilename(Part part) {
+        for (String cd : part.getHeader("content-disposition").split(";")) {
+            if (cd.trim().startsWith("filename")) {
+                String filename = cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");
+                return filename.substring(filename.lastIndexOf('/') + 1).substring(filename.lastIndexOf('\\') + 1); // MSIE fix.
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @return the foto
+     */
+    public String getFoto() {
+        return foto;
+    }
+
+    /**
+     * @param foto the foto to set
+     */
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+    
+}
